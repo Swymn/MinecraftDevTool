4a6eff175993f404245a4e731ae8744d89b8337a
Swynn <venture.ugo@gmail.com>
2024-09-14 19:36:13 +0200

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #[allow(dead_code)]
 fn get_template_generator(type_str: &str) -> TemplateGeneratorType {
-	match type_str {
-		"spigot" => TemplateGeneratorType::Spigot(SpigotTemplateGenerator),
-		_ => panic!("Unknown template generator type: {}", type_str),
-	}
+    match type_str {
+        "spigot" => TemplateGeneratorType::Spigot(SpigotTemplateGenerator),
+        _ => panic!("Unknown template generator type: {}", type_str),
+    }
 }
 
 #[allow(dead_code)]
 enum TemplateGeneratorType {
-	Spigot(SpigotTemplateGenerator),
+    Spigot(SpigotTemplateGenerator),
 }
 
 #[derive(Default)]
@@ -16,17 +16,17 @@ struct SpigotTemplateGenerator;
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn get_template_generator_return_template_default() {
-		// GIVEN a a string type;
-		let type_str = "spigot";
+    #[test]
+    fn get_template_generator_return_template_default() {
+        // GIVEN a a string type;
+        let type_str = "spigot";
 
-		// WHEN get_template_generator is called with the type_str;
-		let result = get_template_generator(type_str);
-		
-		// THEN the result should be a SpigotTemplateGenerator;
-		assert!(matches!(result, TemplateGeneratorType::Spigot(_)));
+        // WHEN get_template_generator is called with the type_str;
+        let result = get_template_generator(type_str);
+
+        // THEN the result should be a SpigotTemplateGenerator;
+        assert!(matches!(result, TemplateGeneratorType::Spigot(_)));
     }
 }
