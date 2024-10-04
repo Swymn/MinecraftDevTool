d6820b7be4ce61e9334cd73e08bbbf2c413dd306
Swymn <venture.ugo@gmail.com>
2024-10-04 22:31:06 +0200

    fix env variable system

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,28 @@ use std::io::BufReader;
 use std::{env, io};
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let mut args: Vec<String> = env::args().skip(1).collect();
     let mut input_buffer = BufReader::new(io::stdin());
 
-    let project_type = get_parameters(&args, &mut input_buffer, "Please provide the project type:");
-
+    let project_type = get_parameters(&mut args, &mut input_buffer, "Please provide the project type:");
+    
     let project_generator = get_project_type(&project_type);
 
     if let Some(project_generator) = project_generator {
         match project_generator {
             ProjectGeneratorType::Spigot => {
                 let name = get_parameters(
-                    &args,
+                    &mut args,
                     &mut input_buffer,
                     "Please provide a name for your project",
                 );
                 let version = get_parameters(
-                    &args,
+                    &mut args,
                     &mut input_buffer,
                     "Please provide a version for your project",
                 );
                 let group_id = get_parameters(
-                    &args,
+                    &mut args,
                     &mut input_buffer,
                     "Please provide a group id for your project",
                 );
--- src/parameter_reader.rs
+++ src/parameter_reader.rs
@@ -10,13 +10,15 @@ fn get_user_input<B: io::BufRead>(buffer_reader: &mut B, input: &'static str) ->
 }
 
 pub fn get_parameters<B: io::BufRead>(
-    args: &[String],
+    args: &mut Vec<String>,
     buffer_reader: &mut B,
     input: &'static str,
 ) -> String {
-    args.first()
-        .cloned()
-        .unwrap_or_else(|| get_user_input(buffer_reader, input))
+    if !args.is_empty() {
+        args.remove(0)
+    } else {
+        get_user_input(buffer_reader, input)
+    }
 }
 
 #[cfg(test)]
@@ -24,18 +26,18 @@ mod tests {
     use super::*;
     use std::io::Cursor;
 
-    fn setup_test(args: Vec<String>, input: &str) -> String {
+    fn setup_test(args: &mut Vec<String>, input: &str) -> String {
         let mut buffer_reader = Cursor::new(input);
-        get_parameters(&args, &mut buffer_reader, "")
+        get_parameters(args, &mut buffer_reader, "")
     }
 
     #[test]
     fn get_parameters_should_return_parameter_from_one_args() {
         // GIVEN an args array with one value
-        let args = vec![String::from("toto")];
+        let mut args = vec![String::from("toto")];
 
         // WHEN we call the get_parameter function
-        let parameter = setup_test(args, "\n");
+        let parameter = setup_test(&mut args, "\n");
 
         // THEN the parameter returned should be 'toto'
         assert_eq!("toto", parameter);
@@ -44,14 +46,14 @@ mod tests {
     #[test]
     fn get_parameters_should_return_parameter_from_many_args() {
         // GIVEN an args array with multiple values
-        let args = vec![
+        let mut args = vec![
             String::from("toto"),
             String::from("tata"),
             String::from("titi"),
         ];
 
         // WHEN we call the get_parameter function
-        let parameter = setup_test(args, "\n");
+        let parameter = setup_test(&mut args, "\n");
 
         // THEN the parameter should be 'toto'
         assert_eq!("toto", parameter)
@@ -60,10 +62,10 @@ mod tests {
     #[test]
     fn get_parameters_should_return_parameter_from_user_input_because_of_empty_args() {
         // GIVEN an empty args array
-        let args: Vec<String> = Vec::new();
+        let mut args: Vec<String> = Vec::new();
 
         // WHEN we call the get_parameter function
-        let parameter = setup_test(args, "toto\n");
+        let parameter = setup_test(&mut args, "toto\n");
 
         // THEN the parameter should be 'toto'
         assert_eq!("toto", parameter);
